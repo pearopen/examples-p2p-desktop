abd32305e65af3bef7a5dc91154f7499fca86125
pear.open.dev <pear.open.dev@gmail.com>
2025-12-27 09:13:13 +1100

    init

---
+++ basic-live-cam/README.md
@@ -0,0 +1,33 @@
+# Basic Live Cam
+
+- P2P basic live cam app running in desktop environment
+
+- Stack: holepunch, bare, pear, corestore, hyperswarm, blind-pairing, hyperdb, hrpc, autobase, hyperblobs, hypercore-blob-server, bare-ffmpeg
+
+## Usage
+
+```shell
+npm i
+npm run build
+
+# user1: create room + print invite
+pear run --store /tmp/user1 . --name user1
+
+# user2: join room
+pear run --store /tmp/user2 . --name user2 --invite <invite>
+```
+
+## Build Pear app
+```shell
+npm i
+npm run build
+
+pear stage <channel>
+pear seed <channel>
+
+# user1: create room + print invite
+pear run --store /tmp/user1 <pear-link> --name user1
+
+# user2: join room
+pear run --store /tmp/user2 <pear-link> --name user2 --invite <invite>
+```
---
+++ basic-live-cam/schema.js
@@ -0,0 +1,110 @@
+import Hyperschema from 'hyperschema'
+import HyperdbBuilder from 'hyperdb/builder'
+import Hyperdispatch from 'hyperdispatch'
+import HRPC from 'hrpc'
+
+const SCHEMA_DIR = './spec/schema'
+const DB_DIR = './spec/db'
+const DISPATCH_DIR = './spec/dispatch'
+const HRPC_DIR = './spec/hrpc'
+
+const hyperSchema = Hyperschema.from(SCHEMA_DIR)
+const schema = hyperSchema.namespace('basic-live-cam')
+schema.register({
+  name: 'writer',
+  fields: [
+    { name: 'key', type: 'buffer', required: true }
+  ]
+})
+schema.register({
+  name: 'invite',
+  fields: [
+    { name: 'id', type: 'buffer', required: true },
+    { name: 'invite', type: 'buffer', required: true },
+    { name: 'publicKey', type: 'buffer', required: true },
+    { name: 'expires', type: 'int', required: true }
+  ]
+})
+schema.register({
+  name: 'video',
+  fields: [
+    { name: 'id', type: 'string', required: true },
+    { name: 'name', type: 'string', required: true },
+    { name: 'type', type: 'string', required: true },
+    { name: 'blob', type: 'json', required: true },
+    { name: 'info', type: 'json' }
+  ]
+})
+schema.register({
+  name: 'videos',
+  array: true,
+  type: '@basic-live-cam/video'
+})
+schema.register({
+  name: 'message',
+  fields: [
+    { name: 'id', type: 'string', required: true },
+    { name: 'text', type: 'string', required: true },
+    { name: 'info', type: 'json' }
+  ]
+})
+schema.register({
+  name: 'messages',
+  array: true,
+  type: '@basic-live-cam/message'
+})
+schema.register({
+  name: 'add-message',
+  fields: [
+    { name: 'text', type: 'string', required: true },
+    { name: 'info', type: 'json' }
+  ]
+})
+Hyperschema.toDisk(hyperSchema)
+
+const hyperdb = HyperdbBuilder.from(SCHEMA_DIR, DB_DIR)
+const db = hyperdb.namespace('basic-live-cam')
+db.collections.register({
+  name: 'invites',
+  schema: '@basic-live-cam/invite',
+  key: ['id']
+})
+db.collections.register({
+  name: 'videos',
+  schema: '@basic-live-cam/video',
+  key: ['id']
+})
+db.collections.register({
+  name: 'messages',
+  schema: '@basic-live-cam/message',
+  key: ['id']
+})
+HyperdbBuilder.toDisk(hyperdb)
+
+const hyperdispatch = Hyperdispatch.from(SCHEMA_DIR, DISPATCH_DIR, { offset: 0 })
+const dispatch = hyperdispatch.namespace('basic-live-cam')
+dispatch.register({ name: 'add-writer', requestType: '@basic-live-cam/writer' })
+dispatch.register({ name: 'add-invite', requestType: '@basic-live-cam/invite' })
+dispatch.register({ name: 'add-video', requestType: '@basic-live-cam/video' })
+dispatch.register({ name: 'add-message', requestType: '@basic-live-cam/message' })
+Hyperdispatch.toDisk(hyperdispatch)
+
+const hrpc = HRPC.from(SCHEMA_DIR, HRPC_DIR)
+const rpc = hrpc.namespace('basic-live-cam')
+rpc.register({
+  name: 'videos',
+  request: { name: '@basic-live-cam/videos', send: true }
+})
+rpc.register({
+  name: 'add-video',
+  request: { name: 'string', send: true }
+})
+rpc.register({
+  name: 'messages',
+  request: { name: '@basic-live-cam/messages', send: true }
+})
+rpc.register({
+  name: 'add-message',
+  request: { name: '@basic-live-cam/add-message', send: true }
+})
+HRPC.toDisk(hrpc)
